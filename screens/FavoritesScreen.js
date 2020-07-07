904585d5b8311c5def12debdb23491afd78ec5da
Joshua Tan <jtanjoshua@gmail.com>
2020-07-07 15:46:39 +0800

    fallback text for favorites screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -6,9 +6,22 @@ import { useSelector } from "react-redux";
 const FavoritesScreen = (props) => {
   const favoritedFoods = useSelector((state) => state.food.favoriteFoods);
 
+  if (favoritedFoods.length === 0 || !favoritedFoods) {
+    return (
+      <View style={styles.message}>
+        <Text>No favorited food found</Text>
+      </View>
+    );
+  }
   return <FoodList navigation={props.navigation} foods={favoritedFoods} />;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  message: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default FavoritesScreen;
